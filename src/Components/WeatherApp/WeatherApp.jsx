00fd5e382cedac4d4cd23bdf5df897c3ed3ead7d
Rohit Kumar Anchal <105239963+rohitanchal@users.noreply.github.com>
2023-10-24 23:48:00 +0530

    done at api call in variable

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -11,12 +11,44 @@ import humidity_icon from "../Assets/humidity.png";
 
 
 const WeatherApp = () => {
+    let api_key = "852a795d17a9ccc038756312148f53b8";
+    const search = () => {
+        const element = document.getElementsByClassName("cityInput")
+        if (element[0].value === "") {
+            return 0;
+        }
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&appid=852a795d17a9ccc038756312148f53b8`;
+
+    }
     return (
         <div className='container'>
             <div className='top-bar'>
                 <input type="text" className="cityInput" placeholder='Search' />
-                <div className="search-icon">
+                <div className="search-icon" onClick={() => { search() }}>
                     <img src={search_icon} alt="" />
+                </div>
+            </div>
+            <div className="weather-image">
+                <img src={cloud_icon} alt="" />
+            </div>
+            <div className="weather-temp">24°c</div>
+            <div className="weather-location">London</div>
+            <div className="data-container">
+
+                <div className="element">
+                    <img src={humidity_icon} alt="" className="icon" />
+                    <div className="data">
+                        <div className="humidity-percent">64%</div>
+                        <div className="text">Humidity</div>
+                    </div>
+                </div>
+
+                <div className="element">
+                    <img src={wind_icon} alt="" className="icon" />
+                    <div className="data">
+                        <div className="humidity-percent">18 km/h</div>
+                        <div className="text">Wind Speed</div>
+                    </div>
                 </div>
             </div>
         </div>
